bfe01104b1f9aaeae2135be8cec1c762509ddf59
Ahmed Khelifi <47308272+ahmed-khlifi@users.noreply.github.com>
2022-03-28 03:27:36 -0700

    Update FrenchInactive.tsx

--- FrenchInactive.tsx
+++ FrenchInactive.tsx
@@ -5,9 +5,9 @@ function FrenchInactive(props) {
   return (
     <Svg
       xmlns="http://www.w3.org/2000/svg"
-      width={61}
-      height={61}
-      viewBox="0 0 61 61"
+      width={60}
+      height={60}
+      viewBox="0 0 60 60"
       {...props}
     >
       <G data-name="Group 5413">
